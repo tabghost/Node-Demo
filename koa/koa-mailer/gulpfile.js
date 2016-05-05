5fe97af0d6d9721f178fac0395139213297ba2fb
sharlly <874875702@qq.com>
2016-05-05 22:08:02 +0800

    koa-mailer first commit

---
+++ koa/koa-mailer/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	concat = require('gulp-concat'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename');
+
+// 编译less
+gulp.task('build-less', function() {
+	gulp.src('./public/less/*.less')
+		.pipe(less({ compress: true }))
+		.on('error', function(e) {console.log(e);})
+		.pipe(gulp.dest('./public/css'));
+});
+
+// 合并压缩
+gulp.task('min-concat', ['build-less'], function() {
+	gulp.src(['./public/css/*.css'])
+		// 合并为all.css并输出
+		.pipe(concat('all.css'))
+		.pipe(gulp.dest('./public/css/'))
+		// 重命名为all.min.css并输出
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('dev', function() {
+	gulp.watch('./public/less/*.less', ['build-less', 'min-concat']);
+});
